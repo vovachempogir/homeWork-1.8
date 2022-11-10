c4735768355d608e416b3d2af4abcf474875e685
Vladimir <vovachempogir@mail.ru>
2022-11-11 03:55:42 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        System.out.println(" Задача 1");
+        int [] weights = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0 };
+        weights [0] = 90;
+        int januaryWeight = weights [0];
+        System.out.println(januaryWeight);
+        System.out.println(weights[0]);
+        System.out.println(weights[4]);
+        System.out.println(weights[11]);
+        int january = 0;
+        System.out.println(weights[january]);
+        for (int i = 0; i < weights.length; i++) {
+            System.out.println(weights[i]);
+        }
+
+    }
+}
