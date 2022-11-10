e1a394ed891ecd75b17e70253c02084ae9093957
Vladimir <vovachempogir@mail.ru>
2022-11-11 05:13:05 +0700

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         // Задача 1
-        System.out.println(" Задача 1");
-        int [] weights = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0 };
-        weights [0] = 90;
-        int januaryWeight = weights [0];
-        System.out.println(januaryWeight);
-        System.out.println(weights[0]);
-        System.out.println(weights[4]);
-        System.out.println(weights[11]);
-        int january = 0;
-        System.out.println(weights[january]);
-        for (int i = 0; i < weights.length; i++) {
-            System.out.println(weights[i]);
+        System.out.println(" Задача 1 ");
+        double[] arr = new double[3];
+        arr [0] = 1.57;
+        arr [1] = 7.654;
+        arr [2] = 9.986;
+        System.out.println(" Задача 2");
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[0] + ", " + arr[1] + ", " + arr[2]);
+        System.out.println(" Задача 3 ");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+        System.out.println(" Задача 4");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
         }
 
     }
-}
